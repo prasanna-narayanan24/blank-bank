437d26b8422a88f04c93549a3f986ed9ef0baecc
Prasanna <prasanna.narayanan2021@gmail.com>
2019-10-19 19:13:02 +0530

    Updates

---
+++ precache-manifest.ab008985ba603145eb31f85ed13e7d90.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f74543967815fc622316576c15b9d7",
+    "url": "/blank-bank/index.html"
+  },
+  {
+    "revision": "8cf46e0638b018ed03a4",
+    "url": "/blank-bank/static/js/2.37a3fe53.chunk.js"
+  },
+  {
+    "revision": "738bb1c73b0aba23610f",
+    "url": "/blank-bank/static/js/main.7934d2bf.chunk.js"
+  },
+  {
+    "revision": "5cf32c406213c4623d1a",
+    "url": "/blank-bank/static/js/runtime-main.3d924c19.js"
+  }
+]);
